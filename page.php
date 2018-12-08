eacc694ea06282059dc97b0ef7f3ab259a98d58b
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2018-12-07 22:34:12 -0500

    commit

--- page.php
+++ page.php
@@ -1,57 +1,4 @@
 <?php get_header(); ?> 
-
-<body>
-	<!-- LOGO -->
-	<header>
-		<div class="container justify-content-center">
-			<div class="p-3 text-center">
-				<?php if (has_custom_logo()): ?>
-					<?php  the_custom_logo();?>			
-				<?php else: ?>
-					<h3 class="text-uppercase"> <?php bloginfo( 'name' ); ?> </h3>
-				<?php endif ?>
-			</div>
-		</div>
-		<!-- PREHEADER -->
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 col-md-6 col-12 text-md-left text-center align-middle title-page">
-					<?php the_title(); ?>
-				</div>
-				<div class="col-lg-6 col-md-6 col-12 align-middle text-center">
-					<ul class="nav justify-content-md-end justify-content-lg-end  justify-content-center my-3 mt-lg-0 mt-md-0 auth-section">
-						<li class="nav-item"><a class="nav-link" href="#">Ingreso</a></li>
-						<li class="nav-item"><a class="nav-link" href="#">Registrarse</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-
-		<!-- NAV -->
-
-		<div class="container-fluid text-center" id="main-nav">
-			<div class="container">
-				<nav class="nav-bar navbar-expand-md navbar-dark justify-content-center">
-					<!-- <a class="navbar-brand" href="#">Navbar</a> -->
-
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-					 aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<?php 
-						$args = [
-							'theme_location' => 'main',
-							'container_class' => 'collapse navbar-collapse',
-							'container_id' => 'navbarNav',
-							'menu_class' => 'navbar-nav',
-							'walker' => new bootstrap_4_walker_nav_menu()
-						];
-						wp_nav_menu($args); 
-					?>
-				</nav>	
-			</div>
-		</div>
-	</header>
 	<div id="main-container" class="container pb-4">
 		
 		<!-- WIDGETS PREFOOTER -->
@@ -72,16 +19,24 @@
 	</div>
 			
 	<main class="container my-0 p-0">
-		<div class="row">
-			<div class="col-12 p-0">
 			<?php if (have_posts()): ?>
 			<?php while(have_posts()): the_post(); ?>
-				<?php the_content() ?>
-			<?php endwhile; ?>
+			<div class="row">
+				<div class="col-12 p-0">
+					<?php the_post_thumbnail('full',['class' => 'img-fluid']); ?>
+				</div>
 			</div>
+			<div class="row mt-4">
+				<div class="col-12 col-lg-9 col-md-8">
+					<?php the_content() ?>
+				</div>
+				<div class="col-12 col-lg-3 col-md-4 p-2">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+			<?php endwhile; ?>
 		<?php else: ?>
 			<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 		<?php endif; ?>
-		</div>
 	
 <?php get_footer(); ?>
